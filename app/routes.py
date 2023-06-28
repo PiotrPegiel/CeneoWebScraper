5e5cb9cd34819a41201f1ffd929ad211a218e865
PiotrPegiel <p.a.pegiel@gmail.com>
2023-06-28 02:56:45 +0200

    2023-06-28 PP Scenariusz 4

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, send_from_directory, current_app, send_file
 import os
 import json
 import requests
@@ -23,8 +23,9 @@ def extract():
             page_dom = BeautifulSoup(response.text, "html.parser")
             opinions = page_dom.select("div.js_product-review")
             if not opinions:
-                error = 'Błędny kod lub nie znaleziono opinii'
+                error = 'Błędny kod, nie znaleziono opinii, lub Ceneo mówi "NIE"'
                 return render_template('extract.html', error=error)
+            name = page_dom.select_one("h1.product-top__product-info__name").text
             for opinion in opinions:
                 single_opinion = {}
                 for key, value in selectors.items():
@@ -34,15 +35,32 @@ def extract():
                 url = "https://www.ceneo.pl" + extract_tag(page_dom, "a.pagination__next", "href")
             except TypeError:
                 url = None
+        # Zapisywanie opinii do pliku
         with open(f"./app/data/opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
             json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+
+        # !Analiza danych!
+        # Odczytanie pliku
+        data_path = f"./app/data/opinions/"
+        file_name = f"{product_code}.json"
+        file_path = os.path.join(data_path, file_name)
+        opinions = pd.read_json(file_path)
+        # Zamiana ',' na '.'
+        opinions.rating = opinions.rating.map(lambda x: float(x.split("/")[0].replace(",", ".")))
+        # Przypisanie statystyk
+        product = {'name': name, 'path': file_path, 'url': url, 'opinions_count': opinions.shape[0],
+                   'pros_count': opinions.pros.map(bool).sum(), 'cons_count': opinions.cons.map(bool).sum(),
+                   'avg_rating': opinions.rating.mean().round(2)}
+
+
+        # Przejście na stronę produktu
         return redirect(url_for('product', product_code=product_code))
     return render_template('extract.html')
 
 @app.route('/product/<product_code>')
 def product(product_code):
     opinions = pd.read_json(f"./app/data/opinions/{product_code}.json")
-    return render_template('product.html', product_code=product_code, opinions=opinions.to_html(header=1, classes='table table-striped table-success', table_id='opinions'))
+    return render_template('product.html', product_code=product_code, opinions=opinions.to_html(header=1, classes='w-full text-sm text-left text-gray-500 dark:text-gray-400', table_id='opinions'))
 
 @app.route('/products')
 def products():
@@ -50,15 +68,38 @@ def products():
     data_path = f"./app/data/opinions/"
     file_names = [filename for filename in os.listdir(data_path) if filename.endswith('.json')]
     for file_name in file_names:
-        # with open(os.path.join(data_path, file_name), encoding='utf-8') as file:
-        #     opinions = json.load(file)
-        #     print(len(opinions))
-        opinions = pd.read_json(os.path.join(data_path, file_name))
-        print(opinions)
-        opinions_count = opinions.shape[0]
-        pros_count = int(opinions.pros.map(bool).sum())
-        print(pros_count)
-    return render_template('products.html')
+        file_path = os.path.join(data_path, file_name)
+        opinions = pd.read_json(file_path)
+        opinions.rating = opinions.rating.map(lambda x: float(x.split("/")[0].replace(",",".")))
+        code = file_name.split(".")
+        url = f"https://www.ceneo.pl/{code[0]}"
+
+        # error handling in case of "Ceneo wpierdala się z rowerka"
+        response = requests.get(url)
+        page_dom = BeautifulSoup(response.text, "html.parser")
+        name = page_dom.select_one("h1.product-top__product-info__name")
+        if not name:
+            name = code[0]
+        else:
+            name = name.text
+
+        # Przypisanie danych
+        product = {
+            'name': name,
+            'code': code[0],
+            'path': file_path,
+            'url': url,
+            'opinions_count': opinions.shape[0],
+            'pros_count': opinions.pros.map(bool).sum(),
+            'cons_count': opinions.cons.map(bool).sum(),
+            'avg_rating': opinions.rating.mean().round(2)}
+        all_products.append(product)
+    return render_template('products.html', list=all_products)
+
+# Pobieranie wskazanych plików przez products
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(f'data/opinions', filename, as_attachment=True)
 
 @app.route('/author')
 def author():
